66ec20dfe32611392553c888f7113c517dd61a59
zhongkai yu <45715644+zhongkaiyu@users.noreply.github.com>
2024-11-11 12:04:18 -0800

    Update run.sh
    new loop parameters

--- run.sh
+++ run.sh
@@ -8,41 +8,37 @@ rm -f host_output.txt
 #     sudo ./build/numa_benchmark 1 100000 $block >> output.txt
 # done
 
-blocksize_init=64
-blocksize_upper=$(echo "64 * 1024^2" | bc)
-totaldata_init=$(echo "64 * 1024^2" | bc)
-totaldata_upper=$(echo "4 * 1024^3" | bc)
+blocksize_init=8
+blocksize_upper=$(echo "2 * 1024" | bc)
+arraysize_init=$(echo "16 * 1024" | bc)
+arraysize_upper=$(echo "16 * 1024^2" | bc)
+# arraysize_init=$(echo "64 * 1024^2" | bc)
+# arraysize_upper=$(echo "4 * 1024^3" | bc)
 
-# blocksize_init=64
-# blocksize_upper=$(echo "128" | bc)
-# totaldata_init=$(echo "64 * 1024^2" | bc)
-# totaldata_upper=$(echo "128 * 1024^2" | bc)
 
 
 blocksize=$blocksize_init
 while [ $blocksize -le $blocksize_upper ]; do
-
-    totaldata=$totaldata_init
-    while [ $totaldata -le $totaldata_upper ]; do
-        arraysize=$((totaldata / blocksize))
-        # echo $blocksize $totaldata $arraysize
-        numactl --cpunodebind=0 --membind=0 
-        sudo ./build/numa_benchmark 2 $arraysize $blocksize >> cxl_output.txt
-        totaldata=$((totaldata * 2))
+    arraysize=$arraysize_init
+    while [ $arraysize -le $arraysize_upper ]; do
+        # arraysize=$((arraysize / blocksize))
+        # echo $blocksize $arraysize $arraysize
+        # sudo numactl --cpunodebind=0 
+        sudo numactl --cpunodebind=0 --membind=2 ./build/numa_benchmark 2 $arraysize $blocksize >> cxl_output.txt
+        arraysize=$((arraysize * 2))
     done
     blocksize=$((blocksize * 2))
 done
 
 blocksize=$blocksize_init
 while [ $blocksize -le $blocksize_upper ]; do
-
-    totaldata=$totaldata_init
-    while [ $totaldata -le $totaldata_upper ]; do
-        arraysize=$((totaldata / blocksize))
-        # echo $blocksize $totaldata $arraysize
-        numactl --cpunodebind=0 --membind=0 
-        sudo ./build/numa_benchmark 0 $arraysize $blocksize >> host_output.txt
-        totaldata=$((totaldata * 2))
+    arraysize=$arraysize_init
+    while [ $arraysize -le $arraysize_upper ]; do
+        # arraysize=$((arraysize / blocksize))
+        # echo $blocksize $arraysize $arraysize
+        # sudo numactl --cpunodebind=0 
+        sudo numactl --cpunodebind=0 --membind=0 ./build/numa_benchmark 0 $arraysize $blocksize >> host_output.txt
+        arraysize=$((arraysize * 2))
     done
     blocksize=$((blocksize * 2))
 done
